bd752af7fef039369c92bc93fc98fedef10a4791
Jade Riel <90788116+Jaderiel@users.noreply.github.com>
2022-04-04 06:48:47 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,12 +8,21 @@ if (isset($_GET['id'])) {
 
     $sql = "DELETE FROM `users` WHERE `id`='$user_id'";
 
-     $result = $conn->query($sql);
+    $result = $conn->query($sql);
+
+    $sql2 = "DELETE FROM `user_add` WHERE `id`='$user_id'";
+
+    $result2 = $conn->query($sql2);
 
      if ($result == TRUE) {
 
         echo "Record deleted successfully.";
         header ("Location: view.php");
+
+        if ($result2 == TRUE) {
+
+            echo "Record deleted successfully.";
+            header ("Location: view.php");
 
     }else{
 
@@ -21,6 +30,7 @@ if (isset($_GET['id'])) {
 
     }
 
-} 
+}
+}
 
 ?>
